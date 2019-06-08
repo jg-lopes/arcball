679eb5d6b73af2b5d99eaeb96010644f7c6ea864
João Gabriel Lopes <jglopes6@gmail.com>
2019-06-08 19:48:49 -0300

    Implemented Rubiks Cube colors for cube

--- arcball.js
+++ arcball.js
@@ -1,21 +1,33 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+
+// Colors of the face of the cube (following a Rubik's Cube model)
+// Blue - FRONT // Green - BACK
+// Yellow - UP // White - DOWN
+// Red - RIGHT // Orange - LEFT
+var colors = [ 'red', 'orange', 'yellow', 'white', 'blue', 'green'];
+
+for ( var i = 0; i < geometry.faces.length; i += 2 ) {
+    var color = new THREE.Color( colors[i/2] );
+    geometry.faces[ i ].color.set(color);
+    geometry.faces[ i + 1 ].color.set( color);
+}
+
+var material = new THREE.MeshBasicMaterial( { color: 0xffffff, vertexColors: THREE.FaceColors } );
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+camera.position.z = 3;
 
 var animate = function () {
     requestAnimationFrame( animate );
 
-    cube.rotation.x += 0.01;
+    //cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
 
     renderer.render( scene, camera );
