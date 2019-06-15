94c679de27be38c6767487bbc8e10c9db5663e15
João Gabriel Lopes <jglopes6@gmail.com>
2019-06-15 16:41:29 -0300

    Added dynamic arcball selection

--- arcball.js
+++ arcball.js
@@ -4,12 +4,16 @@ var mouse = new THREE.Vector2();
 var isClicking;
 var frustumSize = 2;
 
-
+var mouseVector = new THREE.Vector3();
 var lastMouse = new THREE.Vector2();
 var curMouse = new THREE.Vector2();
 var quaternion = new THREE.Quaternion();
 
-var interactiveBox;
+var interactiveBoxes;
+
+var currentClicked;
+var activeArcball;
+
 
 init();
 animate();
@@ -33,7 +37,7 @@ function init() {
 
 
     // Introducing the cube + arcball visualization object
-    interactiveBox = new THREE.Object3D();
+    interactiveBoxes = new THREE.Group();
 
     // Cube
     var geometry = new THREE.BoxGeometry( 1, 1, 1);
@@ -47,21 +51,50 @@ function init() {
     }
     var material = new THREE.MeshBasicMaterial( { color: 0xffffff, vertexColors: THREE.FaceColors } );
     var box = new THREE.Mesh( geometry, material );
-    interactiveBox.add( box );
 
-    // Sphere
-    var sphereGeom = new THREE.SphereGeometry(1, 100, 100);
-    var blueMaterial = new THREE.MeshBasicMaterial( { color: 0x000000, transparent: true, opacity: 0.3 } );
-    var sphere = new THREE.Mesh( sphereGeom, blueMaterial );
-    interactiveBox.add(sphere);
+    box.scale.set(0.5, 0.5, 0.5);
+    box.translateX(-0.5);
+    box.translateY(-0.5);
+    box.translateZ(-10);
+
+    box.rotateX(Math.PI/5);
+    box.rotateY(Math.PI/5);
+
+    interactiveBoxes.add( box );
+
+    // Cube
+    var geometry = new THREE.BoxGeometry( 1, 1, 1);
+    // Colors of the face of the cube (following a Rubik's Cube model)
+    // Blue - FRONT // Green - BACK // Yellow - UP // White - DOWN // Red - RIGHT // Orange - LEFT
+    var colors = [ 'red', 'orange', 'yellow', 'white', 'blue', 'green'];
+    for ( var i = 0; i < geometry.faces.length; i += 2 ) {
+        var color = new THREE.Color( colors[i/2] );
+        geometry.faces[ i ].color.set(color);
+        geometry.faces[ i + 1 ].color.set( color);
+    }
+    var material = new THREE.MeshBasicMaterial( { color: 0xffffff, vertexColors: THREE.FaceColors } );
+    var box2 = new THREE.Mesh( geometry, material );
+
+    box2.scale.set(0.75, 0.75, 0.75);
+    box2.translateX(0.3);
+    box2.translateY(-0.2);
+    box2.translateZ(-5);
+
+
+    box2.rotateY(Math.PI/5);
+    box2.rotateZ(Math.PI/5);
+
+    interactiveBoxes.add( box2 );
+
+    scene.add( interactiveBoxes );
+
+    // // Sphere
+    // var sphereGeom = new THREE.SphereGeometry(1, 100, 100);
+    // var blueMaterial = new THREE.MeshBasicMaterial( { color: 0x000000, transparent: true, opacity: 0.3 } );
+    // var sphere = new THREE.Mesh( sphereGeom, blueMaterial );
+    // interactiveBox.add(sphere);
     
-    scene.add(interactiveBox);
-
-
-    interactiveBox.scale.set(0.5, 0.5, 0.5);
-    interactiveBox.translateX(-0.5);
-    interactiveBox.translateY(-0.5);
-    interactiveBox.translateZ(-10);
+    // scene.add(interactiveBox);
 
 
 
@@ -70,6 +103,9 @@ function init() {
     document.addEventListener( 'mouseup', onDocumentMouseUp, false);
     document.addEventListener( 'mousemove', onDocumentMouseMove, false );
     window.addEventListener( 'resize', onWindowResize, false );
+    document.addEventListener( 'mousewheel', onDocumentMouseWheel, false );
+
+
 
     document.body.appendChild( renderer.domElement );
 }
@@ -113,31 +149,71 @@ function getArcballVector(mouseEvent, object) {
 
 function onDocumentMouseDown(event) {
     isClicking = true;
-    
-    //console.log(experimentalBall(mouseEvent, offset, scale));
+
+    // for (var i = 0; i < interactiveBoxes.children.length; i++) {
+    //     var box = interactiveBoxes.children[i];
+    //     if (! isEmpty (box.children)) {
+    //         scene.remove(box.children[0]);
+    //     }
+    //     console.log(interactiveBoxes.children[i]);
+    // }
+
+
+    raycaster.setFromCamera(mouseVector, camera);
+
+    var intersects = raycaster.intersectObjects(scene.children, true);
+
+    if (! isEmpty(intersects)) {
+        
+
+        var sphereGeom = new THREE.SphereGeometry(1, 100, 100);
+        var blueMaterial = new THREE.MeshBasicMaterial( { color: 0x000000, transparent: true, opacity: 0.3 } );
+        activeArcball = new THREE.Mesh( sphereGeom, blueMaterial );
+        
+        activeArcball.name = 'arcball';
+        objScale = intersects[0].object.scale;
+        objPosition = intersects[0].object.position;
+        activeArcball.scale.set(objScale.x, objScale.y, objScale.z);
+        activeArcball.position.set(objPosition.x, objPosition.y, objPosition.z);
+
+        scene.remove(scene.getObjectByName('arcball'));
+        scene.add(activeArcball);
+        
+    } else {
+        scene.remove(scene.getObjectByName('arcball'));
+    }
+
+    //console.log(intersects);
+
 }
 
 function onDocumentMouseUp(event) {
     isClicking = false;
 }
 
-
 function onDocumentMouseMove( event ) {
 
+    mouseVector.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouseVector.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    
     curMouse.x = event.clientX;
     curMouse.y = event.clientY;
     
     if (isClicking) {
        
 
-        va = getArcballVector(lastMouse, interactiveBox);
-        vb = getArcballVector(curMouse, interactiveBox);
+        raycaster.setFromCamera(mouseVector, camera);
 
-        var angle = Math.acos(Math.min(1, va.dot(vb) / va.length() /vb.length()));
+        //var intersects = raycaster.intersectObject(activeArcball);
+        //console.log(intersects);
+        va = getArcballVector(lastMouse, currentClicked);
+        vb = getArcballVector(curMouse, currentClicked);
+
+        var angle = Math.acos(Math.min(1, va.dot(vb) / va.length() / vb.length()));
         var axis = va.cross(vb).normalize();
 
         quaternion.setFromAxisAngle(axis, angle);
-        interactiveBox.quaternion.multiplyQuaternions(quaternion, interactiveBox.quaternion);
+        currentClicked.quaternion.multiplyQuaternions(quaternion, currentClicked.quaternion);
 
         
     }
@@ -145,6 +221,10 @@ function onDocumentMouseMove( event ) {
 
     lastMouse.x = curMouse.x;
     lastMouse.y = curMouse.y;
+}
+
+function onDocumentMouseWheel( event ) {
+    camera.zoom += event.wheelDeltaY * 0.05;
 }
 
 
@@ -163,6 +243,16 @@ function onWindowResize() {
 
 function animate() {
     requestAnimationFrame( animate );
+
+    
+
     renderer.render( scene, camera );
 }
 
+function isEmpty(obj) {
+    for(var key in obj) {
+        if(obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+}
